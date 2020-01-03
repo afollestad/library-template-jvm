b7af0f566653abede8c5647361aac8754405b65a
Aidan Follestad <afollestad@squareup.com>
2020-01-02 18:19:53 -0800

    Fix sample package

--- sample/src/main/java/com/afollestad/librarytemplatesample/Main.kt
+++ sample/src/main/java/com/afollestad/librarytemplatesample/Main.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.afollestad.statemachinesample
+package com.afollestad.librarytemplatesample
 
 import com.afollestad.librarytemplate.StuffDoer
 import java.util.Scanner
